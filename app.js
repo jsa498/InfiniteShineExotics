579af633f9914be6323cd45229e7bee1d8e26b3a
jsa498 <jaskaran1431085@gmail.com>
2024-09-03 20:18:51 -0700

    Update app.js

--- app.js
+++ app.js
@@ -98,7 +98,7 @@ btnNav.onclick = function() {
 btnAbout.onclick = function() {
   modal.style.display = "block";
 }
-
+ 
 // When the user clicks on <span> (x), close the modal
 span.onclick = function() {
   modal.style.display = "none";
@@ -120,4 +120,3 @@ document.getElementById("callNow").onclick = function() {
 document.getElementById("messageNow").onclick = function() {
   window.location.href = "sms:+16047258010";
 }
-
